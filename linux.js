80c9763b07b7f5aa2140c2523291224d244ae061
Suyash Dahitule <you@example.com>
2024-12-31 19:06:12 +0530

    add inital files for backend

---
+++ linux.js
@@ -0,0 +1,9 @@
+/**
+ * git init
+ * touch file name
+ * hidden file ls-a
+ * git status => tracking file status
+ * to add => git add .
+ * dlete or restore previos
+ * git restore --staged filename.txt
+ */
